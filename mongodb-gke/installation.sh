a8835ce63efe181b35f2a984371b24e2026d6346
George Alonge <georgedelson@gmail.com>
2023-03-21 21:43:45 -0700

    docs: start installation steps

--- mongodb-gke/installation.sh
+++ mongodb-gke/installation.sh
@@ -18,5 +18,3 @@
   # flag maps the node ports 31000 to 31005 to the host machine
 ###########################
 k3d cluster create demo-cluster --agents 2 --port 31000-31005:31000-31005@loadbalancer
-
-aclea
